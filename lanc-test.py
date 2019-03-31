76d97a1db8a868bd69928bb70a5a02cbcf5abb27
enesbcs <lwolf@freemail.hu>
2019-03-31 10:35:21 +0200

    Fixing LSB bit order

--- lanc-test.py
+++ lanc-test.py
@@ -2,8 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 
-RecordStart = 0x1833
-RecordStop  = 0x8C19
+RecordStart = [0x18,0x33]
+RecordStop  = [0x8C,0x19]
 
 def bin(s):
     return str(s) if s<=1 else bin(s>>1) + str(s&1)
@@ -39,34 +39,39 @@ class LANC:
     return time_passed
 
  def sendLanc(self,repeats):
-    while (pulseIn(self.rpin,1)<5000):
-     pass # sync to next LANC message
+    while (pulseIn(self.rpin,1)<5000): # sync to next LANC message
+     pass
     while repeats>0:
      for bytenr in range(8):
-      time.sleep(self.bitduration)
+      time.sleep(self.bitduration)     # LOW after long pause means the START bit of Byte 0 is here, wait START bit duration
       for i in range(8*(bytenr+1)):
        if bytenr<2:
-        if self.lancCmd[i]:
+        if self.lancCmd[i]:            # During the first two bytes the adapter controls the line and puts out data
          GPIO.output(self.spin,1)
         else:
          GPIO.output(self.spin,0)
        time.sleep(self.halfbitduration)
-       self.lancMessage[i] = GPIO.input(self.rpin)
+       self.lancMessage[i] = GPIO.input(self.rpin) # Read data line during middle of bit
        time.sleep(self.halfbitduration)
       GPIO.output(self.spin,0)
-      time.sleep(self.halfbitduration)
-      while GPIO.input(self.rpin)==1:
+      time.sleep(self.halfbitduration)             # Make sure to be in the stop bit before waiting for next byte
+      while GPIO.input(self.rpin)==1:              # Loop as long as the LANC line is +5V during the stop bit or between messages
        pass
      repeats = repeats - 1
 
- def createcmdarray(self,cmd):
-  tc = str(bin(cmd))
+ def createcmdarray(self,cmd):  # This function fills the lancCmd array with the bits in the order they should be sent
   for i in range(len(self.lancCmd)):
    self.lancCmd[i] = 0
-  l = 0
-  for i in reversed(range(len(tc))):
-   self.lancCmd[l] = int(tc[i])
-   l=l+1
+  for i in range(len(cmd)):
+   bs = str(bin(cmd[i]))
+   ba = (i*8)
+   L = 0
+   for ls in reversed(range(len(bs))):     # First byte 1 then byte 2 but with LSB first for both bytes
+    try:
+     self.lancCmd[ba+L] = int(bs[ls])
+    except:
+     self.lancCmd[ba+L] = 0
+    L=L+1
 
  def displaymessage(self):
   print(self.lancMessage)
